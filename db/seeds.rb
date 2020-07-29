f799492ae2c12e4b4af2acc43ff585abfb545d9b
David Kennell <davidkennell0@gmail.com>
2020-07-29 08:09:36 -0500

    Add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Room.create(name: 'Music', description: 'Gimme sumadem funki tunes')
+Room.create(name: 'Politics', description: "I'm right, you're wrong")
+Room.create(name: 'Programming', description: "if (!programmer){pls leave}")
+
+User.create(email: "qwer@qwer.com", password: "password")
+
+Message.create(user_id: 1, room_id: 1, body: 'Skadoooosh')
