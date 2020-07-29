8e526d70ba6e687158bab9860e2cf3df3f16e8cc
David Kennell <davidkennell0@gmail.com>
2020-07-29 14:37:26 -0500

    Get messages showing up in chat box with alternating colors

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,7 +12,22 @@ consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log("Recieving:")
+    console.log("Receiving:")
     console.log(data.content)
+    let div = buildChatMessage(data.content)
+    document.querySelector('#msg').appendChild(div)
   }
 });
+
+function buildChatMessage(content) {
+  const div = document.createElement('div')
+  // We want to alternate background colors for our messages
+  const lastMessage = document.querySelector('#msg').lastElementChild
+  const lastMessageBackgroundColor = lastMessage.style.backgroundColor
+  if (!lastMessageBackgroundColor){
+    div.style.backgroundColor = "#ffe5ba"
+  }
+  div.innerHTML = content
+  div.classList.add("p-3")
+  return div
+}
