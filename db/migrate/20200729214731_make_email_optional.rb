d85be8205c257a0713c315169f8ee749311d635a
David Kennell <davidkennell0@gmail.com>
2020-08-09 17:06:28 -0500

    Make 'make email option' migration reversible

--- db/migrate/20200729214731_make_email_optional.rb
+++ db/migrate/20200729214731_make_email_optional.rb
@@ -1,6 +1,6 @@
 class MakeEmailOptional < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :email
+    remove_column :users, :email, :string
     add_column :users, :email, :string, optional: true
   end
 end
